012a82b8e4f7f8842ff925027dba06dd14aa1764
xming13 <xiaoming33@gmail.com>
2015-05-09 17:20:45 +0800

    New layout

--- app/js/spot-the-special-one.js
+++ app/js/spot-the-special-one.js
@@ -269,6 +269,14 @@ XMing.GameStateManager = new function() {
             confirmButtonColor: '#EAF53B'
         });
 
+        $(".mfb-component__button--main").click(function() {
+            if ($(this).parent().parent().attr('data-mfb-state') === "open") {
+                $(".overlay").addClass("active");
+            } else {
+                $(".overlay").removeClass("active");
+            }
+        });
+
         $(".btn-play").click(function() {
             self.startGame();
         });
